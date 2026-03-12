3494dd0fe8defd63d8eb07c8e0fc1274e8dc6c82
Carol <carolzyx1118@outlook.com>
2026-03-12 10:21:25 +0800

    Route tRPC through api/rpc

---
+++ api/rpc.ts
@@ -0,0 +1,6 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+import runHandler from "./_runHandler";
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  return runHandler(req, res);
+}
